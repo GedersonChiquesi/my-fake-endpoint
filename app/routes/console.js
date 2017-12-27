46549fe3cb50cd77e140d1e224350fef93932046
Gederson Chiquesi <gedersonchiquesi@hotmail.com>
2017-12-27 14:29:09 -0200

    Change formate date and sort in console

--- app/routes/console.js
+++ app/routes/console.js
@@ -1,7 +1,7 @@
 module.exports = function (app, db) {
   app.get('/console', (req, res) => {
       database = db.db('heroku_b10ghscl');
-      database.collection('results').find({},{body:1})
+      database.collection('results').find({},{body:1}).sort({date: -1})
         .toArray((err, result) => {
           if (err) return console.log(err);
           res.render('console', { quotes: result, title: 'Lista de request' });
--- app/routes/message.js
+++ app/routes/message.js
@@ -1,14 +1,14 @@
 module.exports = function (app, db,jsonParser) {
-var dateformat = require('dateformat')
+const dateformat = require('dateformat')
   app.post('/message', jsonParser, (req, res) => {
-    var json = { "body": req.body, "date": dateformat(Date(),'d/m/yyyy hh:MM:ss TT'), "path":req.path };
+    let json = { "body": req.body, "headers": req.headers, "date": dateformat(Date(),'d/m/yyyy UTC:hh:MM:ss TT'), "path":req.path };
     database = db.db('heroku_b10ghscl');
     database.collection('results').insertOne(json, (err,res)=>{
       if (err) throw err;
       console.log(`Number of documents inserted: ${res.insertedCount}`);
     });
     
-    res.send(json, 201, Date());
+    res.send(json.body, 201, Date());
   });
 
 }
